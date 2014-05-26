a799b17f570563ac57c3ef0061f99f4c03c700a6
Karel Vuong <karelvuong@outlook.com>
2014-05-25 22:43:36 -0400

    Removed /

--- js/app.js
+++ js/app.js
@@ -19,43 +19,43 @@ angular.module('dimensions', [
     .state('social', {
         url: "/social",
         controller: "SocialController",
-        templateUrl: "/partials/social/_social.html",
+        templateUrl: "partials/social/_social.html",
     })
     .state('social.facebook', {
         url: "/facebook",
-        templateUrl: "/partials/social/facebook.html",
+        templateUrl: "partials/social/facebook.html",
     })
     .state('social.googleplus', {
         url: "/googleplus",
-        templateUrl: "/partials/social/googleplus.html",
+        templateUrl: "partials/social/googleplus.html",
     })
     .state('social.instagram', {
         url: "/instagram",
-        templateUrl: "/partials/social/instagram.html",
+        templateUrl: "partials/social/instagram.html",
     })
     .state('social.linkedin', {
         url: "/linkedin",
-        templateUrl: "/partials/social/linkedin.html",
+        templateUrl: "partials/social/linkedin.html",
     })
     .state('social.pinterest', {
         url: "/pinterest",
-        templateUrl: "/partials/social/pinterest.html",
+        templateUrl: "partials/social/pinterest.html",
     })
     .state('social.twitter', {
         url: "/twitter",
-        templateUrl: "/partials/social/twitter.html",
+        templateUrl: "partials/social/twitter.html",
     })
     .state('social.vimeo', {
         url: "/vimeo",
-        templateUrl: "/partials/social/vimeo.html",
+        templateUrl: "partials/social/vimeo.html",
     })
     .state('social.youtube', {
         url: "/youtube",
-        templateUrl: "/partials/social/youtube.html",
+        templateUrl: "partials/social/youtube.html",
     })
     .state('mobile', {
         url: "/mobile",
         controller: "MobileController",
-        templateUrl: "/partials/mobile/_mobile.html",
+        templateUrl: "partials/mobile/_mobile.html",
     })
 });
