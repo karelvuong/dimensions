856699140fa4acee419609c00ce9d8f50f0217fd
Karel Vuong <karelvuong@outlook.com>
2014-05-25 23:02:35 -0400

    Reverted social back to its own folder.

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ angular.module('dimensions', [
     .state('social', {
         url: "/social",
         controller: "SocialController",
-        templateUrl: "partials/_social.html",
+        templateUrl: "partials/social/_social.html",
     })
     .state('social.facebook', {
         url: "/facebook",
